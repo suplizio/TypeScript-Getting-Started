0c4a5034c7c17072ffe9ad78c547544866e082e1
Brice Wilson <brice@5wilsons.net>
2017-01-31 08:51:33 -0500

    more ts stuff

--- app/player.ts
+++ app/player.ts
@@ -1,6 +1,6 @@
 export class Player {
 
-    constructor(public playerName: string = '') { }
+    constructor(public playerName: string = '', public age: number) { }
 
     logPlayer(): void {
         console.log('The current player is ' + this.playerName + '.');
