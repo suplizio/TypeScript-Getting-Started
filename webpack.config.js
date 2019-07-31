401fd2c8e0bfb4a1d050075cb0bd163f0d1d172c
Brice Wilson <brice@5wilsons.net>
2019-07-31 17:31:01 -0400

    cleaned up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  devServer: {
-    lazy: true,
+  entry: './app/app.ts',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
     filename: 'bundle.js'
+  },
+  devServer: {
+    quiet: true,
+    inline: false
   }
 };
