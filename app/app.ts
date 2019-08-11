6d60b1be46327fa108f0dc0b19f328dfedeb5ae3
Brice Wilson <brice@5wilsons.net>
2019-08-11 13:09:02 -0400

    recorded demo 2

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,31 @@
 function startGame() {
   // starting a new game
 
-  let playerName: string = 'Audrey';
+  let playerName: string | undefined = getInputValue('playername');
   logPlayer(playerName);
 
-  let messagesElement = document.getElementById('messages');
-  
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
+  postScore(80, playerName);
 }
 
-function logPlayer(name: string): void {
+function logPlayer(name: string = 'MultiMath Player'): void {
   console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+  if (inputElement.value === '') {
+    return undefined;
+  }
+  else {
+    return inputElement.value;
+  }  
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+  const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+  scoreElement!.innerText = `${score} - ${playerName}`;  
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
