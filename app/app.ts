880d9cbe2bc907afe9341db0adca2a19fe4072b3
Brice Wilson <brice@5wilsons.net>
2019-08-11 12:52:32 -0400

    recorded demo 1

--- app/app.ts
+++ app/app.ts
@@ -9,7 +9,7 @@ function startGame() {
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
 }
 
-function logPlayer(name) {
+function logPlayer(name: string): void {
   console.log(`New game starting for player: ${name}`);
 }
 
