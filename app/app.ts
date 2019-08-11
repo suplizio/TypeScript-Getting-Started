3dc16af8e265e2c67ad0ccac9fba13fdc0785660
Brice Wilson <brice@5wilsons.net>
2019-08-11 13:26:36 -0400

    recorded demo 4

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,7 @@ function startGame() {
   logPlayer(playerName);
 
   postScore(80, playerName);
+  postScore(-5, playerName);
 }
 
 function logPlayer(name: string = 'MultiMath Player'): void {
@@ -24,12 +25,26 @@ function getInputValue(elementID: string): string | undefined {
 }
 
 function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+
+  let logger: (value: string) => void;
+
+  if (score < 0) {
+    logger = logError;
+  }
+  else {
+    logger = logMessage;
+  }
+
   const scoreElement: HTMLElement | null = document.getElementById('postedScores');
   scoreElement!.innerText = `${score} - ${playerName}`;  
+
+  logger(`Score: ${score}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
 
 const logMessage = (message: string) => console.log(message);
 
-logMessage('Welcome to MultiMath!');
+function logError(err: string): void {
+  console.error(err);
+}
