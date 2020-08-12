f185f2457f24f9dac70ae68f8740b7fcb0fc0afe
Jason Suplizio <suplizio@hotmail.com>
2020-08-12 14:14:09 -0700

    basic typing

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 /** Start a new game **/
 function startGame() {
     var messagesElement = document.getElementById('messages');
+    
+    const playerName: string = "Audrey";
+    logPlayer(playerName);
+
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+function logPlayer(name: string) {
+    console.log(`New game starting for player: ${name}`)
+}
