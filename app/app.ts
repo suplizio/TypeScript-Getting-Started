f14efbc2c739cf26369ac0a95f23fce16ae48b42
Jason Suplizio <suplizio@hotmail.com>
2020-08-11 19:15:47 -0700

    base tsconfig file, config & snippet for webpack

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,4 @@
+/** Start a new game **/
 function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   entry: './app/app.ts',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
     filename: 'bundle.js'
