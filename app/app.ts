7f3b25975a22b2106a48c30019a16852f3cc4d4e
Jason Suplizio <suplizio@hotmail.com>
2020-08-12 17:56:49 -0700

    builtin type demos

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,62 @@
+
+//Parameters with default value are implicitly optional
+function logPlayer(name: string = "MultiMath Player"): void {
+    console.log(`New game starting for player: ${name}`)
+}
+
+//Return type of string or undefined
+function getInputValue(elementID: string): string | undefined {
+    let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    return (inputElement.value === '') ? undefined : inputElement.value;
+}
+
+//Parameters with default value are implicitly optional
+function postScore(score: number, playerName: string = "MultiMath Player"): void {
+
+    //logger can be assigned to any function with string parameter that returns void
+    let logger: (value: string) => void;
+
+    logger = (score < 0 || undefined) ? logError : logMessage;
+    
+    logger('Score: ' +score);
+
+    const scoreElement: HTMLElement | null = <HTMLElement>document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+
+}
+
+//Anonymous Arrow function
+let logMessage = (msg: string) => console.log(msg);
+
+function logError(msg: string): void {
+    console.error(msg);
+}
+
 /** Start a new game **/
 function startGame() {
-    var messagesElement = document.getElementById('messages');
-    
-    const playerName: string = "Audrey";
+    let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
 
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    postScore(100);
+
+    let messagesElement = document.getElementById('messages');
+    let message = `Welcome ${playerName} to MultiMath! Starting new game...`;
+    messagesElement!.innerText = message;
+    logMessage(message);
+
+    postScore(100, playerName);
+    postScore(-5);
+
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
 
-function logPlayer(name: string) {
-    console.log(`New game starting for player: ${name}`)
+// TypeAssertion(a.k.a. casting)
+function typeAssertionFun() {
+    let value: any = 5;
+    let valueString: string = (<number>value).toFixed(4);
+    console.log(valueString)
+
+    let fixedString: String = (value as number).toFixed(4);
+    console.log(fixedString);
 }
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+function typeFun() {
+    var value = 5;
+    var valueString = value.toFixed(4);
+    console.log(value);
+}
+//# sourceMappingURL=tempCodeRunnerFile.js.map
